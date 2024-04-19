5fd0893f8b141243f5dbcc05670a3f098de5501a
unknown <mineomi1234@gmail.com>
2024-04-19 17:43:26 +0200

    Orders and history tab

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,52 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  // interval: any;
+  constructor(/*private http: HttpClient*/) {
+  //   this.interval = setInterval(() => this.getData(), 1000);
+   }
+
+  // readonly ROOT_URL = 'http://localhost:8080/getLastOrders'
+
+  // orders :any
+
+  
+
+  // getData(): any {
+  //   console.log("cos")
+  //   let response = this.http.get(this.ROOT_URL);
+  //   response.subscribe((data) => {
+  //     this.orders = data;
+  //   });
+  // }
+
+  
+
+  orders = [
+    { 
+      name: 'Jakub',
+      surname: "Ciołek",
+      className: '3TP',
+      first: false,
+      second: true,
+      timeStamp: "19.04 15:29:12"
+    },
+    { 
+      name: 'Maciej',
+      surname: "Wróbel",
+      className: '4TP',
+      first: true,
+      second: true,
+      timeStamp: "19.04 15:28:58"
+    },
+    { 
+      name: 'Maciej',
+      surname: "Nowicki",
+      className: '4TP',
+      first: false,
+      second: false,
+      timeStamp: "19.04 15:28:47"
+    },
+  ]
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,56 @@ export class Tab2Page {
 
   constructor() {}
 
+  orders = [
+    { 
+      name: 'Jakub',
+      surname: "Ciołek",
+      className: '3TP',
+      first: false,
+      second: true,
+      timeStamp: "19.04 15:29:12"
+    },
+    { 
+      name: 'Maciej',
+      surname: "Wróbel",
+      className: '4TP',
+      first: true,
+      second: true,
+      timeStamp: "19.04 15:28:58"
+    },
+    { 
+      name: 'Maciej',
+      surname: "Nowicki",
+      className: '4TP',
+      first: false,
+      second: false,
+      timeStamp: "19.04 15:28:47"
+    },
+    { 
+      name: "Anna",
+      surname: "Kowalska",
+      className: "2TP",
+      first: true,
+      second: false,
+      timeStamp: "19.04 15:30:05"
+    },
+    { 
+      name: "Katarzyna",
+      surname: "Lewandowska",
+      className: "5TP",
+      first: false,
+      second: true,
+      timeStamp: "19.04 15:31:20"
+    },
+    { 
+      name: "Piotr",
+      surname: "Nowak",
+      className: "3TP",
+      first: true,
+      second: true,
+      timeStamp: "19.04 15:32:10"
+    }
+    
+  ]
+
 }
